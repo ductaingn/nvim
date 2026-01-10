f30caab8647c2ff5abd70b542dba648b66a7bced
R4C00n <ductaingn.015203@gmail.com>
2026-01-10 15:23:10 +0700

    rainbow brackets

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      float = {
+        transparent = true,
+      },
+    },
+  },
+}
