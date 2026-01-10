39776cd9cd325997d684f61966af704eff4471ba
R4C00n <ductaingn.015203@gmail.com>
2026-01-10 10:13:15 +0700

    transparent background

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true, -- <- key option
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
